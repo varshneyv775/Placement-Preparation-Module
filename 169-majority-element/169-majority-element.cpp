63fcee6f321c893b652ccdba5a61f8a3eb469b8f
varshneyv775 <74603970+varshneyv775@users.noreply.github.com>
2022-08-08 22:32:36 +0530

    Time: 29 ms (58.53%), Space: 19.4 MB (92.27%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& a) {
+        int n=a.size();
+        int c=0;
+        int can;
+        for(int i=0;i<n;i++)
+        {
+            if(c==0)
+            {
+                can=a[i];
+            }
+            if(can==a[i])
+            {
+                c++;
+            }
+            else
+            {
+                c--;
+            }
+        }
+        return can; 
+    }
+};
